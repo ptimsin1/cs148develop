cda66d14b24a4ab7d3d46576e3f4f854c93762ea
Prakriti Timsina <ptimsin1@silk3.uvm.edu>
2015-10-28 18:16:18 -0400

    home page working

---
+++ misc/index.php
@@ -0,0 +1,18 @@
+<?php
+
+//##############################################################################
+//
+// main home page for the site 
+// 
+//##############################################################################
+include "top.php";
+
+// Begin output
+print '<article>';
+print '<h2>Welcome!</h2>';
+print '<p> Thanks for coming to this page! Click on "Display Table"to see the '
+.'classes that we offer here at UVM or to see the classes we will be offering.</p>';
+print '</article>';
+include "footer.php";
+
+?>
