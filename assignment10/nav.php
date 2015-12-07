fc885e3fd992344362a70e01bc812a230c3a697c
Prakriti Timsina <ptimsin1@silk3.uvm.edu>
2015-12-07 17:15:29 -0500

    cheking nav to see if it works

--- assignment10/nav.php
+++ assignment10/nav.php
@@ -87,17 +87,17 @@
             //}
        // }      
         
-           for($i =0; $i<1; $i++)
-            {
-                
-               if($username == $adminId[$i])
-               {
-                    if ($path_parts['filename'] == "Employment") {
-                       print '<li class="activePage"><button>Employment</button></li>';
-       }           else {
-                       print '<li><a href="employment.php"><button>Employment</button></a></li>';        }
-                }
-            }
+//           for($i =0; $i<1; $i++)
+//            {
+//                
+//               if($username == $adminId[$i])
+//               {
+//                    if ($path_parts['filename'] == "Employment") {
+//                       print '<li class="activePage"><button>Employment</button></li>';
+//       }           else {
+//                       print '<li><a href="employment.php"><button>Employment</button></a></li>';        }
+//                }
+//            }
         }
       
         ?>
